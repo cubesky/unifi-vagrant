8a54bcc41289218bac35fa794268970c53a2800e
cubesky <max_301@live.com>
2018-09-06 00:03:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update
 apt-get install mc vim htop ncdu 
-wget -c -N http://dl.ubnt.com/unifi/4.7.6/unifi_sysvinit_all.deb
+wget -c -N http://dl.ubnt.com/unifi/5.7.23/unifi_sysvinit_all.deb
 dpkg -i unifi_sysvinit_all.deb
 apt-get -f install -y
 
